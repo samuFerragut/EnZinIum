49647021cb690e37eaf5615ec9df2a62201363d9
samuFerragut <sferragut@cifpfbmoll.eu>
2020-02-13 18:08:41 +0100

    fix(README): Rehacer toString

--- src/main/java/enzinium/Address.java
+++ src/main/java/enzinium/Address.java
@@ -5,57 +5,36 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 
 public class Address {
+    private PrivateKey SK;
+    private PublicKey PK;
+    private double Balance = 0d;
+    private String Symbol = "EZI";
 
-    private PublicKey PK = null;
-    private PrivateKey SK = null;
-    private double balance = 0d;
-    private String symbol = "EZI";
-
-    public Address() {
-    }
-
-    public void setSK(PrivateKey sKey) {
-        this.SK = sKey;
-    }
-
-    public PrivateKey getSK() {
-        return this.SK;
-    }
-
-    public void setPK(PublicKey pKey) {
-        this.PK = pKey;
+    public void Address(int PK, int SK, int Balance, String Symbol) {
     }
 
     public PublicKey getPK() {
-        return this.PK;
+        this.PK = PK;
+        return PK;
     }
 
     public double getBalance() {
-        return this.balance;
+        this.Balance = Balance;
+        return Balance;
     }
 
-    public void generateKeyPair() {
-        KeyPair pair = GenSig.generateKeyPair();
-        this.setSK(pair.getPrivate());
-        this.setPK(pair.getPublic());
+    public PrivateKey getSK() {
+        this.SK = SK;
+        return SK;
     }
 
-    public void transferEZI(double enziniums) {
-        this.balance += enziniums;
-    }
-
-    public void send(TokenContract contract, Double enziniums) {
-        if (enziniums <= this.balance) {
-            contract.payable(getPK(), enziniums);
-            this.balance -= enziniums;
-        }
+    public String getSymbol() {
+        this.Symbol = Symbol;
+        return Symbol;
     }
 
     @Override
     public String toString() {
-        return "\n" + "PK = " + getPK().hashCode() + "\n" + 
-                      "Balance = " + getBalance() + " " 
-                      + this.symbol + "\n";
+        return "PublicKey: " + getPK() + "symbol: " + getSymbol() + "balance: " + getBalance();
     }
-
 }
--- src/test/java/enzinium/AddressTest.java
+++ src/test/java/enzinium/AddressTest.java
@@ -2,40 +2,19 @@ package enzinium;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
+import org.junit.Assert;
 import org.junit.Test;
+import enzinium.Address;
 
 public class AddressTest {
 
-    /**
-     * Test SK y PK
-     */
     @Test
-    public void constructor_test() {
+    public void toStringTest(){
         Address address = new Address();
-        assertNotNull(address);
-        assertEquals(0d, address.getBalance(), 0d);
-    }
-
-    @Test
-    public void generate_key_pair_test() {
-        Address address = new Address();
-        assertNotNull(address);
-        address.generateKeyPair();
-        assertNotNull(address.getPK());
-        assertNotNull(address.getSK());
-    }
-
-
-    @Test
-    public void transferEZI_test() {
-
-        Address rick = new Address();
-        rick.generateKeyPair();
-
-        rick.transferEZI(20d);
-        rick.transferEZI(20d);
-
-        assertEquals(40d, rick.getBalance(), 0d);
+        System.out.println(address.toString());
     }
 }
